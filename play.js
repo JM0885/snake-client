230b34a9c78854db56ca2784963932b3f78f4e63
Jared Mosher <jared.mosher@gmail.com>
2020-12-02 23:23:50 +0000

    Refactored to move stdin functions to input.js file

--- play.js
+++ play.js
@@ -1,25 +1,8 @@
-const net = require('net');
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log(data)
-  })
-  conn.on('connect', () => {
-    console.log('Successfully connected.')
-  })
-  
-    return conn;
-}
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
+connect(); //THIS IS THE CONNECTION OBJECT (call setupInput in play.js, )
+//function(function());
 
-
+setupInput();
