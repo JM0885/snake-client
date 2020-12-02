35129302027eee95ee28221a3dcf1c2a59ee8114
Jared Mosher <jared.mosher@gmail.com>
2020-12-02 21:31:12 +0000

    added name to the file

--- client.js
+++ client.js
@@ -8,11 +8,13 @@ const connect = function() {
   conn.setEncoding('utf8'); 
   conn.on('data', (data) => {
     console.log(data)
-  })
+  });
   conn.on('connect', () => {
-    console.log('Successfully connected.')
-  })
-  
+    console.log('Successfully connected to game server.')
+  }); 
+  conn.on('connect', () => {
+    conn.write('Name: JDM');
+  });
     return conn;
 }
 
