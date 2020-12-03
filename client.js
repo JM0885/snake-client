46be58b4077cff27dc3b697ffe5ee4b7b80b55fc
Jared Mosher <jared.mosher@gmail.com>
2020-12-03 01:17:21 +0000

    updates

--- client.js
+++ client.js
@@ -12,10 +12,10 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server.')
     conn.write('Name: JDM');
-    setTimeout(function(){conn.write('Move: up')}, 300);
-    setTimeout(function(){conn.write('Move: up')}, 600);
-    setTimeout(function(){conn.write('Move: up')}, 900);
-    setTimeout(function(){conn.write('Move: up')}, 1200);
+    // setTimeout(function(){conn.write('Move: up')}, 300);
+    // setTimeout(function(){conn.write('Move: up')}, 600);
+    // setTimeout(function(){conn.write('Move: up')}, 900);
+    // setTimeout(function(){conn.write('Move: up')}, 1200);
   }); 
 
   conn.on('data', (data) => {
